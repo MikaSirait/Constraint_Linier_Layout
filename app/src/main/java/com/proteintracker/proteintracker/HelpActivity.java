476dbb1a298a02fca8454b740167308b37ff1784
Mika Sirait <mika.sirait@si.ukdw.ac.id>
2019-09-04 21:15:33 +0700

    Initial commit

---
+++ app/src/main/java/com/proteintracker/proteintracker/HelpActivity.java
@@ -0,0 +1,24 @@
+package com.proteintracker.proteintracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
+
+super.onCreate(savedInstanceState);
+        TextView myTextView = new TextView(this);
+        myTextView.setText("Some Help Text");
+        setContentView(myTextView);
+        TextView myTextView = new TextView(this);
+        Bundle b = getIntent().getExtras();
+        String helpText = b.getString("helpString");
+        myTextView.setText(helpText);
+        setContentView(myTextView);
